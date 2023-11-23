560252189b767f1c5dd969cea2e962aac65e6abd
Zabbasisocio <zabbasi85@gmail.com>
2023-11-23 11:50:12 +0000

    Samnple convo

--- T5convertor_variation.py
+++ T5convertor_variation.py
@@ -4,13 +4,25 @@ import nltk
 import string
 from nltk.corpus import stopwords
 from nltk.tokenize import word_tokenize, sent_tokenize
-from gensim.summarization.summarizer import summarize
 from gensim import corpora
 from gensim.models.ldamodel import LdaModel
+from transformers import T5ForConditionalGeneration, T5Tokenizer
 
 # Download required NLTK resources
 nltk.download('punkt')
 nltk.download('stopwords')
+
+# Initialize T5
+model_name = "t5-small"
+t5_model = T5ForConditionalGeneration.from_pretrained(model_name)
+#t5_tokenizer = T5Tokenizer.from_pretrained(model_name)
+t5_tokenizer = T5Tokenizer.from_pretrained(model_name, legacy=True)
+
+def t5_summarize(text, model=t5_model, tokenizer=t5_tokenizer):
+    input_text = "summarize: " + text
+    input_ids = tokenizer.encode(input_text, return_tensors="pt", max_length=512, truncation=True)
+    summary_ids = model.generate(input_ids, max_length=150, min_length=40, length_penalty=2.0, num_beams=4, early_stopping=True)
+    return tokenizer.decode(summary_ids[0], skip_special_tokens=True)
 
 # Load the exclusion list from a file if it exists
 exclusion_list_file = "exclusion_list.pkl"
@@ -32,7 +44,7 @@ filtered_words = [w for w in word_tokens if w.lower() not in stop_words and len(
 
 # Update the dynamic exclusion list based on certain criteria
 word_freq = nltk.FreqDist(filtered_words)
-high_freq_words = {word for word, freq in word_freq.items() if freq > 10}  # threshold set to 10 for demonstration
+high_freq_words = {word for word, freq in word_freq.items() if freq > 10}
 dynamic_exclusion_list.update(high_freq_words)
 
 # Save the updated exclusion list to a file for future use
@@ -53,34 +65,38 @@ topics = lda.print_topics(num_words=15)
 
 # Extract the topic
 topic_words = [word for word in topics[0][1].split('"')[1::2]]
-# Exclude stopwords, punctuation characters, and dynamic exclusion list
 excluded_items = stop_words.union(set(string.punctuation)).union(dynamic_exclusion_list)
 topic = ' '.join(word for word in topic_words if word.lower() not in excluded_items)
 
-# Summarize the conversation
-summary = summarize(conversation, word_count=150)
+# Summarize the conversation using T5
+chunks = [conversation[i:i+512] for i in range(0, len(conversation), 512)]
+summaries = [t5_summarize(chunk) for chunk in chunks]
+final_summary = ' '.join(summaries)
 
 # Identify action items and dates
-action_items = {}
-keywords = ["handle", "take on", "work on", "coordinate", "set up", "present", "launch", "monitor", "use", "meet", "track", "address", "integrate", "share", "add", "give", "we need to"]
+action_items = {"Alex": [], "Jamie": []}
+keywords = ["you'll","you'll handle", "I'll","I'll handle", "handle", "take on", "work on", "coordinate", "set up", "present", "launch", "monitor", "use", "meet", "track", "address", "integrate", "share", "add", "give", "i need to"]
 
 for sentence in sentences:
     if any(keyword in sentence for keyword in keywords):
-        # Split the sentence based on punctuation to get the action item
         parts = sentence.split(".")
         action = parts[0].strip()
         
-        # Identify the person responsible based on the name mentioned in the sentence
         if "Alex" in sentence:
             responsible = "Alex"
-        elif "Jamie" in sentence:
-            responsible = "Jamie"
         else:
-            responsible = "Both"
+            responsible = "Jamie"
         
-        # Add to the action items dictionary
-        action_items[action] = responsible
+        action_items[responsible].append(action)
 
 print(f"Topic: {topic}")
-print(f"Summary: {summary}")
-print(f"Action Items: {action_items}")
+print(f"\nSummary: {final_summary}")
+#print(f"Action Items: {action_items}")
+
+print("Alex's Tasks:")
+for task in action_items["Alex"]:
+    print(f"- {task}")
+
+print("\nJamie's Tasks:")
+for task in action_items["Jamie"]:
+    print(f"- {task}")
